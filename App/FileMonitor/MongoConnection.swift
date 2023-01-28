b971c1335ae47ba49e3cb88b7c1ef912e26e3817
mcarmanize <20862667+mcarmanize@users.noreply.github.com>
2023-01-27 21:48:57 -0700

    cleanup unused code

--- App/FileMonitor/MongoConnection.swift
+++ App/FileMonitor/MongoConnection.swift
@@ -10,8 +10,6 @@ import Foundation
 import Darwin
 import MongoSwift
 import NIOPosix
-import CLibMongoC
-import Proc
 
 
 @objcMembers class MongoConnection: NSObject {
@@ -20,8 +18,6 @@ import Proc
     var jobIdentifier: String
     var jobIdentifierBSON: BSONObjectID
     var client: MongoClient
-    var esfriend: MongoDatabase
-    var esfriendGrid: OpaquePointer
     var runLogs: MongoDatabase
     
     init(initWithConnectionString connectionString: String, jobIdentifier: String) {
@@ -30,23 +26,11 @@ import Proc
         do {
             try self.jobIdentifierBSON = BSONObjectID(self.jobIdentifier)
             self.client = try MongoClient(self.connectionString, using: elg)
-            self.esfriend = self.client.db("esfriend")
-            let clientPointer: UnsafeMutablePointer<MongoClient> = UnsafeMutablePointer(&self.client)
-            self.esfriendGrid = mongoc_client_get_database(OpaquePointer(clientPointer), "esfriend_grid")
             self.runLogs = self.client.db("run_logs")
         } catch {
             print(error.localizedDescription)
             exit(1)
         }
-    }
-    
-    func insertFileWithPath(_ filePath: URL) -> NSString {
-        // this is broken
-        let file_ptr = UnsafeMutablePointer(mutating: filePath.absoluteString)
-        let connection_string_ptr = UnsafeMutablePointer(mutating: connectionString.description)
-        let file_id = upload_file(file_ptr, connection_string_ptr)
-        let fileIdentifier = NSString(utf8String: file_id)
-        return fileIdentifier
     }
     
     func insertEvent(withFileIdentifier fileIdentifier: NSString, eventDescription: NSString) {
